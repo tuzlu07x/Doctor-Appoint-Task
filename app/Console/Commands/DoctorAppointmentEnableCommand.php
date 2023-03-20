d5d45df3b3c2fa563b7f59e885161039aa159b0d
Fatih Tuzlu <fatihtuzlu07@gmail.com>
2023-03-20 15:19:07 +0300

    enable command added.

---
+++ app/Console/Commands/DoctorAppointmentEnableCommand.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Appointment;
+use Illuminate\Console\Command;
+
+class DoctorAppointmentEnableCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'appoinments:enable';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): void
+    {
+        Appointment::whereDate('appointment_date', '<', now())
+            ->orWhere('is_active', true)
+            ->update(['is_active' => false]);
+    }
+}
